75886e6ee7e72e7393d464fa687546970d84de0a
Dennis Tang <dwtang612@gmail.com>
2020-11-06 23:26:34 -0800

    Created Max X

---
+++ Bringing a Gun To a Guard Fight/solution.py
@@ -0,0 +1,7 @@
+def solution(dimensions, yourPos, guardPos, distance):
+    maxX = yourPos[0] + distance
+    maxY = yourPos[1] + distance
+    print(maxX)
+    print(maxY)
+if __name__ == '__main__':
+    solution([3, 2],[1, 1] ,[2, 1] ,4)
