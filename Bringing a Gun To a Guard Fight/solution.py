44007a0071f650c57ae77460dc3ea1ac137a53e8
Dennis Tang <dwtang612@gmail.com>
2020-11-07 02:45:42 -0800

    Partial Complete

--- Bringing a Gun To a Guard Fight/solution.py
+++ Bringing a Gun To a Guard Fight/solution.py
@@ -1,7 +1,12 @@
+import math 
+
 def solution(dimensions, yourPos, guardPos, distance):
     maxX = yourPos[0] + distance
     maxY = yourPos[1] + distance
-    print(maxX)
-    print(maxY)
+    positiveX = math.ceil(maxX/dimensions[0])
+    positiveY = math.ceil(maxY/dimensions[1])
+    print("positiveX:", positiveX)
+    print("positiveY:", positiveY)
+
 if __name__ == '__main__':
     solution([3, 2],[1, 1] ,[2, 1] ,4)
